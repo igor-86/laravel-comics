5edcee898c07598d51703c695c6476ff8583974e
Igor-86 <i.campoli86@gmail.com>
2022-12-15 18:54:21 +0100

    Header and footer include

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,70 @@
 <footer>
 
-    <h2>Footer</h2>
+    <div class="top">
+        <div class="container d-flex">
+            <div class="sx-menu">
+                <div class="footer-menu d-flex">
+                    <div class="comis me-4">
+                        <h4>COMICS</h4>
+                        <ul>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                        </ul>
+                    </div>
+                    <div class="dc me-4">
+                        <h4>DC</h4>
+                        <ul>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                        </ul>
+                    </div>
+                    <div class="site">
+                        <h4>SITES</h4>
+                        <ul>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                            <li>Lorem</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="info">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet facilis numquam obcaecati libero.
+                </div>
+            </div>
+            <div class="dx-menu">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container d-flex justify-content-between">
+            <a class="butt-sign">
+                Sign up now
+            </a>
+            <div class="follow">
+                <h3 class="me-4">Follow us</h3>
+                <div class="social">
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="social" class="me-3">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="social" class="me-3">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="social" class="me-3">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="social" class="me-3">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="social" class="me-3">
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,53 @@
 <header>
-    <h2>Header</h2>
+    <div class="top-menu">
+        <div class="container d-flex justify-content-end">
+            <p class="me-3 mb-0">Visa</p>
+            <p class="mb-0">Additional DC site</p>
+        </div>
+    </div>
+    <div class="body-menu">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li>
+                        <a href="">Character</a>
+                    </li>
+                    <li>
+                        <a href="">Comics</a>
+                    </li>
+                    <li>
+                        <a href="">Movies</a>
+                    </li>
+                    <li>
+                        <a href="">Tv</a>
+                    </li>
+                    <li>
+                        <a href="">Games</a>
+                    </li>
+                    <li>
+                        <a href="">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="">Video</a>
+                    </li>
+                    <li>
+                        <a href="">Fans</a>
+                    </li>
+                    <li>
+                        <a href="">News</a>
+                    </li>
+                    <li>
+                        <a href="">Shop</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="form">
+                <label for="search"></label>
+                <input type="text" placeholder="Search" id="search">
+            </div>
+        </div>
+    </div>
 </header>
